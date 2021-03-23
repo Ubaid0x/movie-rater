59f3e806df857424d5291f0419f359cc6d31980c
Ubaid Iqbal <ubaid95iqbal@gmail.com>
2021-03-24 01:21:39 +0500

    login api added

--- src/api-service.js
+++ src/api-service.js
@@ -1,6 +1,16 @@
 const token = "f8756581ceddad34e71c34bd4b7885f4024c2236"
 
 export class API {
+    static loginUser(body){
+        return fetch("http://127.0.0.1:8000/auth/", {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify( body )         
+        }).then(res => res.json())
+    }
+
     static updateMovie(mov_id, body){
         console.log({mov_id})
         return fetch(`http://127.0.0.1:8000/api/movies/${mov_id}/`, {
--- src/components/auth.js
+++ src/components/auth.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { API } from '../api-service';
 
 const Auth = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('')
+
+    const loginClicked = () => {
+        if(!username || !password){
+            alert('Please fill all fields')
+        }
+        else{
+            API.loginUser({username, password})
+                .then(res => console.log({res}))
+                .catch(err => console.log({err}))
+        }
+    }
+
     return <div>
-        Auth
+        <label htmlFor={'username'}> Username </label> <br />
+        <input id={'username'} type="text" placeholder={"username"} value={username} 
+            onChange={(e) => setUsername(e.target.value)} /> 
+        <br />
+
+        <label htmlFor={'password'}> Password </label> <br />
+        <input id={'password'} type={"pass"} placeholder={"********"}
+            value={password} onChange={(e) => setPassword(e.target.value)} /> 
+        <br />
+        <button onClick={loginClicked}> Login </button>
     </div>
 }
 
